bc2d7bb59a0b6ef973d65003ea171d4d81b6573a
Gowrish_77 <103709921+Gowrish7@users.noreply.github.com>
2022-12-29 22:36:29 +0530

    Add files via upload
    1 - Simple Pattern
          1 2 3 
          1 2 3
          1 2 3

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+package patternprograms;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = new java.util.Scanner(System.in).nextInt();
+		for(int r=1;r<=3;r++)
+		{
+			for(int c=1;c<=3;c++)
+				System.out.print(c);
+		System.out.println();
+		}
+
+	}
+
+}
